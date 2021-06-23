57c401291e8e46fc18a7e15fb55a99815e056f16
Myriam <my.greder@gmail.com>
2021-06-23 07:51:03 +0200

    methode-JS

--- js/model/Photograph.js
+++ js/model/Photograph.js
@@ -5,9 +5,9 @@ class Photograph{
 displayList(){
     let tagsbox="";
     for(let tag of this.tags){
-    tagsbox+= `<a class="tag" href= "tag-${tag}" id="${tag}"><span>#${tag}</span></a>`   
+    tagsbox+= `<a class="tag" href="tag-${tag}" id="${tag}"><span>#${tag}</span></a>`   
     }
-    return `<div class="photographer-profile">
+    return `<a class="photographer-profile" href="src/photographer-page.html?id=${this.id}">
      <article> 
        <section>
          <img src="src/image/FishEyes_Photos/Sample_Photos/Photographers_ID_Photos/${this.portrait}" id="${this.name}" alt="${this.name}">
@@ -20,6 +20,50 @@ displayList(){
        <div class= "tagsbox">
           ${tagsbox}
        </div>
-    </div>`;       
+    </a>`;       
   }
+
+  displayDetail(){
+       let photographer="";
+       for(let photographer of this photographers){
+       photographerList+= `<div class="photographer" href="photographer-${photographer}" id="${photographer}"
+       ><span>#${photographer}</span></div>` 
+       }
+       return '<div class="user-page" href="src/photographer-page.html?id=${this.id}">
+         <div class ="user-page" id ="243">
+             <h1>${this.name}</h1>
+                 <button href="form-modal.html">Contactez-moi
+                 </button>
+                <br>
+                <h4>${this.city}, ${this.country}</h4><br>
+                 <blockquote>{this.tagline}</blockquote>
+                    <div class= "tagsbox">
+                        ${tagsbox}
+                    </div>
+                  <aside>
+                    <img src="src/image/FishEyes_Photos/Sample_Photos/Photographers_ID_Photos/${this.portrait}" id="${this.name}" alt="${this.name}">
+                  </aside>
+          </div>`;     
+  }
+
+  /*pour afficher le photograph sur la page photographer-page*/
+
+ 
 }
+
+essai :
+
+
+var photographer= {
+  id :243 ,
+  photographerElement: "user-page",
+
+   displayDetail:function({
+     console.log(this.id + " " + this.photographerElement);
+   })
+}
+photographer.displayDetails(); // 243 user-page
+
+
+var myPhotographerDetails =  photographer.displayDetails.bind(photographer);///methode bind peut être réutilisée pour un photograph
+myPhotographerDetails();//243 user-page
